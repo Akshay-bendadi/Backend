6e71c280f171b26c37432d18090a751d28494e47
Akshay-bendadi <akshaybendadi@gmail.com>
2024-11-16 19:44:48 +0530

    about section

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const port = 3000;
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-
+app.get("/about", (req, res) => {
+  res.send("About us section");
+});
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`);
 });
