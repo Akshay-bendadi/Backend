61d3ed62cd233eeaaf3e6239f46de4a8dde13fb1
Akshay-bendadi <akshaybendadi@gmail.com>
2024-11-16 19:56:08 +0530

    changed

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -10,5 +9,5 @@ app.get("/about", (req, res) => {
   res.send("About us section");
 });
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${process.env.PORT}`);
 });
